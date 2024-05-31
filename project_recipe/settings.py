a9550c6b7e4d3d9676a00bde3628727fdaa31f7b
PanLipton <panskillsasda@gmail.com>
2024-05-31 16:04:02 +0300

    Виправив баг в settings.py, зробив міграцію, створив рецепт, створив адміна, оновив main.html, бо рецепти за 2024 рік

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
--- recipe/views.py
+++ recipe/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
-from django.utils import timezone
 
 def main(request):
-    current_year = timezone.now().year
-    recipes = Recipe.objects.filter(created_at__year=current_year)
+    recipes = Recipe.objects.filter(created_at__year=2024)
     return render(request, 'main.html', {'recipes': recipes})
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
