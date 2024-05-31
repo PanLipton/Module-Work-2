f2216dee7678c69b100c8201c3d83940a5ddcd86
PanLipton <panskillsasda@gmail.com>
2024-05-31 15:39:58 +0300

    доданий view для відображення деталей рецепту за його id

--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 from django.utils import timezone
 
@@ -6,3 +6,6 @@ def main(request):
     current_year = timezone.now().year
     recipes = Recipe.objects.filter(created_at__year=current_year)
     return render(request, 'main.html', {'recipes': recipes})
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
