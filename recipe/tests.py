f3210b88a92bad6e4ab572c1e9d57ebec01743e4
PanLipton <panskillsasda@gmail.com>
2024-05-31 15:44:53 +0300

    доданий unit-тест для view main

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe, Category
+from django.utils import timezone
 
-# Create your tests here.
+class RecipeViewsTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Dessert")
+        self.recipe = Recipe.objects.create(
+            title="Test Recipe",
+            description="Test Description",
+            instructions="Test Instructions",
+            ingredients="Test Ingredients",
+            category=self.category,
+            created_at=timezone.now()
+        )
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertContains(response, self.recipe.title)
