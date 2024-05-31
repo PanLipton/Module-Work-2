31b8ec77e889e1a735b203015a7f1094a7a447e7
PanLipton <panskillsasda@gmail.com>
2024-05-31 15:45:49 +0300

    доданий unit-тест для view recipe_detail

--- recipe/tests.py
+++ recipe/tests.py
@@ -20,3 +20,11 @@ class RecipeViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main.html')
         self.assertContains(response, self.recipe.title)
+    def test_recipe_detail_view(self):
+        response = self.client.get(reverse('recipe_detail', args=[self.recipe.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipe_detail.html')
+        self.assertContains(response, self.recipe.title)
+        self.assertContains(response, self.recipe.description)
+        self.assertContains(response, self.recipe.ingredients)
+        self.assertContains(response, self.recipe.instructions)
